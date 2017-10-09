10a8011ee4297efe3b06087a112ab1721047d4c7
Sheilar June <Sheilar@sheilarjune.local>
2017-10-09 14:29:27 +0630

    Initial Commit

---
+++ TipsyCalcApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipsyCalcApp
+//
+//  Created by Sheilar June on 10/9/17.
+//  Copyright © 2017 Sheilar June. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
